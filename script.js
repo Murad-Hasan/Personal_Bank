98624bb71ba7ea4cec01feb3d1165f884ad55e90
Murad-Hasan <muradhasan.7845@gmail.com>
2021-01-27 18:23:00 +0600

    completeBank

--- script.js
+++ script.js
@@ -20,6 +20,9 @@ depositBtn.addEventListener('click', function(){
  const withdrawBtn = document.getElementById('addWithdraw')
  withdrawBtn.addEventListener('click', function(){
    const withdrawNumber = getInputNumber('withdrawAmount')
+
+   updateSpanText('currentWithdraw', withdrawNumber);
+   updateSpanText('currentBalance', -1*withdrawNumber);
     document.getElementById('withdrawAmount').value = '';
  })
 
